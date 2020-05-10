ec2fd364197315bbdf4c9b1f331d45a8bf8df32a
Dong Thanh <dongthanh272@gmail.com>
2020-05-11 00:07:35 +0700

    1. Done

---
+++ 60 Realisation/Source Code/QLNhaSach/app/src/main/java/com/qat/android/qlnhasach/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.qat.android.qlnhasach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 2000);
+    }
+}
