9ba894faf33870ab90c91e1d2fe84c714119c826
Dong Thanh <dongthanh272@gmail.com>
2020-05-16 00:36:24 +0700

    5. Created Navigation Drawer and stuff Fragment

--- 60 Realisation/Source Code/QuanLyNhaSach/app/src/main/java/com/qat/android/quanlynhasach/activity/MainActivity.java
+++ 60 Realisation/Source Code/QuanLyNhaSach/app/src/main/java/com/qat/android/quanlynhasach/activity/MainActivity.java
@@ -1,16 +1,124 @@
 package com.qat.android.quanlynhasach.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.qat.android.quanlynhasach.R;
+import com.qat.android.quanlynhasach.constants.Constants;
+import com.qat.android.quanlynhasach.fragment.CartFragment;
+import com.qat.android.quanlynhasach.fragment.HomeFragment;
+import com.qat.android.quanlynhasach.fragment.SettingsFragment;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+import io.paperdb.Paper;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
+    private String type = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Intent intent = getIntent();
+//        Bundle bundle = intent.getExtras();
+//        if (bundle != null) {
+//            type = getIntent().getExtras().get("Admins").toString();
+//        }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Get username and image from firebase
+//        View headerView = navigationView.getHeaderView(0);
+//        TextView userNameTextView = headerView.findViewById(R.id.user_profile_name);
+//        CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
+//        if (!type.equals("Admins")) {
+//            userNameTextView.setText(Constants.currentOnlineUser.getUsername());
+//            Picasso.get().load(Constants.currentOnlineUser.getImage()).placeholder(R.drawable.img_profile).into(profileImageView);
+//        }
+
+        // Open home fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new HomeFragment())
+                        .commit();
+                break;
+
+            case R.id.nav_cart:
+//                if (!type.equals("Admins")) {
+//
+//                }
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new CartFragment())
+                        .commit();
+                break;
+
+            case R.id.nav_settings:
+//                if (!type.equals("Admins")) {
+//
+//                }
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new SettingsFragment())
+                        .commit();
+                break;
+
+            case R.id.nav_logout:
+                Paper.book().destroy();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+                break;
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
